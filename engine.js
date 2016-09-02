fe0f4350afc23d51097d46a33c05710e09ddbacc
Ilya Ilyankou <ilya.ilyankou@trincoll.edu>
2016-09-02 21:01:33 +0300

    Update styling

--- engine.js
+++ engine.js
@@ -2,209 +2,199 @@
  * Model and controller for the Irregular Verbs web app
  *
  * @author Ilya Ilyankou
- * @version 0.1
+ * @version 0.9
  */
 
-var dictionarySize = 107;
+var index = 0; // index of verb in dictionary
+var form = 0;  // first, second or third (0, 1 or 2 respectively)
+var correct = 0; // counter of correct answers
+var altogether = 0;  // counter of all attempts
+var inputTag = '<input type="text" id="myInput" autocomplete="off">';
 
 // Initializing the dictionary
-v = new Array();
-v[1]=new Array("be","was/were","been","быть");
-v[2]=new Array("beat","beat","beaten","бить");
-v[3]=new Array("become","became","become","становиться");
-v[4]=new Array("begin","began","begun","начинать");
-v[5]=new Array("bend","bent","bent","гнуть");
-v[6]=new Array("bite","bit","bitten","кусать");
-v[7]=new Array("blow","blew","blown","дуть");
-v[8]=new Array("break","broke","broken","ломать, разрушать");
-v[9]=new Array("bring","brought","brought","приносить");
-v[10]=new Array("build","built","built","строить");
-v[11]=new Array("burn","burned/burnt","burned/burnt","гореть");
-v[12]=new Array("burst","burst","burst","лопаться, взрываться");
-v[13]=new Array("buy","bought","bought","покупать");
-v[14]=new Array("catch","caught","caught","ловить");
-v[15]=new Array("choose","chose","chosen","выбирать");
-v[16]=new Array("come","came","come","приходить, прибывать");
-v[17]=new Array("cost","cost","cost","стоить");
-v[18]=new Array("cut","cut","cut","резать");
-v[19]=new Array("dig","dug","dug","копать, рыть");
-v[20]=new Array("do","did","done","делать");
-v[21]=new Array("draw","drew","drawn","рисовать, чертить");
-v[22]=new Array("dream","dreamed/dreamt","dreamed/dreamt","мечтать");
-v[23]=new Array("drink","drank","drunk","пить");
-v[24]=new Array("drive","drove","driven","ездить");
-v[25]=new Array("eat","ate","eaten","есть, поглощать");
-v[26]=new Array("fall","fell","fallen","падать");
-v[27]=new Array("feed","fed","fed","кормить");
-v[28]=new Array("feel","felt","felt","чувствовать");
-v[29]=new Array("fight","fought","fought","драться, сражаться");
-v[30]=new Array("find","found","found","находить");
-v[31]=new Array("fly","flew","flown","летать");
-v[32]=new Array("forget","forgot","forgotten","забывать");
-v[33]=new Array("forgive","forgave","forgiven","прощать");
-v[34]=new Array("freeze","froze","frozen","замерзать");
-v[35]=new Array("get","got","got","получать");
-v[36]=new Array("give","gave","given","давать");
-v[37]=new Array("go","went","gone/been","идти");
-v[38]=new Array("grow","grew","grown","расти");
-v[39]=new Array("hang","hung","hanged/hung","вешать");
-v[40]=new Array("have","had","had","иметь");
-v[41]=new Array("hear","heard","heard","слышать");
-v[42]=new Array("hide","hid","hidden","прятаться");
-v[43]=new Array("hit","hit","hit","ударять");
-v[44]=new Array("hold","held","held","держать");
-v[45]=new Array("hurt","hurt","hurt","причинять боль");
-v[46]=new Array("keep","kept","kept","держать, хранить");
-v[47]=new Array("kneel","knelt","knelt","становиться на колени");
-v[48]=new Array("know","knew","known","знать");
-v[49]=new Array("lay","laid","laid","лежать");
-v[50]=new Array("lead","led","led","вести, влиять, руководить");
-v[51]=new Array("learn","learned/learnt","learned/learnt","учиться");
-v[52]=new Array("leave","left","left","покидать, оставлять");
-v[53]=new Array("lend","lent","lent","одалживать, давать взаймы");
-v[54]=new Array("let","let","let","позволять");
-v[55]=new Array("lie","lay","lain","обманывать, лгать");
-v[56]=new Array("light","lit","lit","освещать");
-v[57]=new Array("lose","lost","lost","терять");
-v[58]=new Array("make","made","made","делать, создавать");
-v[59]=new Array("mean","meant","meant","означать");
-v[60]=new Array("meet","met","met","встречать");
-v[61]=new Array("pay","paid","paid","платить");
-v[62]=new Array("put","put","put","класть");
-v[63]=new Array("read","read","read","читать");
-v[64]=new Array("ride","rode","ridden","ехать верхом");
-v[65]=new Array("ring","rang","rung","звонить");
-v[66]=new Array("rise","rose","risen","восходить, поднимать");
-v[67]=new Array("run","ran","run","бежать");
-v[68]=new Array("say","said","said","говорить, произносить");
-v[69]=new Array("see","saw","seen","видеть");
-v[70]=new Array("sell","sold","sold","продавать");
-v[71]=new Array("send","sent","sent","отправлять");
-v[72]=new Array("set","set","set","устанавливать");
-v[73]=new Array("shake","shook","shaken","трясти");
-v[74]=new Array("shine","shone","shone","светить, озарять");
-v[75]=new Array("shoot","shot","shot","стрелять");
-v[76]=new Array("show","showed","shown","показывать");
-v[77]=new Array("shut","shut","shut","закрывать");
-v[78]=new Array("sing","sang","sung","петь");
-v[79]=new Array("sink","sank","sunk","тонуть, погружаться");
-v[80]=new Array("sit","sat","sat","сидеть");
-v[81]=new Array("sleep","slept","slept","спать");
-v[82]=new Array("slide","slid","slid","скользить");
-v[83]=new Array("smell","smelled/smelt","smelled/smelt","нюхать, пахнуть");
-v[84]=new Array("speak","spoke","spoken","говорить");
-v[85]=new Array("spend","spent","spent","тратить");
-v[86]=new Array("spill","spilled/spilt","spilled/spilt","проливать");
-v[87]=new Array("spoil","spoiled/spoilt","spoiled/spoilt","портить, наносить ущерб");
-v[88]=new Array("stand","stood","stood","стоять");
-v[89]=new Array("steal","stole","stolen","воровать, красть");
-v[90]=new Array("stick","stuck","stuck","вонзать, приклеивать");
-v[91]=new Array("swim","swam","swum","плавать");
-v[92]=new Array("take","took","taken","брать");
-v[93]=new Array("teach","taught","taught","учить");
-v[94]=new Array("tear","tore","torn","рвать");
-v[95]=new Array("tell","told","told","говорить, сообщать");
-v[96]=new Array("think","thought","thought","думать");
-v[97]=new Array("throw","threw","thrown","бросать");
-v[98]=new Array("understand","understood","understood","понимать");
-v[99]=new Array("wake","woke","woken","просыпаться, бодрствовать");
-v[100]=new Array("wear","wore","worn","носить");
-v[101]=new Array("win","won","won","выиграть");
-v[102]=new Array("write","wrote","written","писать");
-v[103]=new Array("can","could","been able","мочь, уметь, быть способным");
-v[104]=new Array("must","had to","had to","должен, обязан");
-v[105]=new Array("bet","bet","bet","делать ставку");
-v[106]=new Array("misunderstand","misunderstood","misunderstood","неправильно понимать");
-v[107]=new Array("broadcast","broadcast","broadcast","вещать (по радио, ТВ)");
-
+var dict =
+[
+	['be', 'was/were', 'been', 'быть'],
+	['beat', 'beat', 'beaten', 'бить'],
+	['become', 'became', 'become', 'становиться'],
+	['begin', 'began', 'begun', 'начинать'],
+	['bend', 'bent', 'bent', 'гнуть'],
+	['bite', 'bit', 'bitten', 'кусать'],
+	['blow', 'blew', 'blown', 'дуть'],
+	['break', 'broke', 'broken', 'ломать, разрушать'],
+	['bring', 'brought', 'brought', 'приносить'],
+	['build', 'built', 'built', 'строить'],
+	['burn', 'burned/burnt', 'burned/burnt', 'гореть'],
+	['burst', 'burst', 'burst', 'лопаться, взрываться'],
+	['buy', 'bought', 'bought', 'покупать'],
+	['catch', 'caught', 'caught', 'ловить'],
+	['choose', 'chose', 'chosen', 'выбирать'],
+	['come', 'came', 'come', 'приходить, прибывать'],
+	['cost', 'cost', 'cost', 'стоить'],
+	['cut', 'cut', 'cut', 'резать'],
+	['dig', 'dug', 'dug', 'копать, рыть'],
+	['do', 'did', 'done', 'делать'],
+	['draw', 'drew', 'drawn', 'рисовать, чертить'],
+	['dream', 'dreamed/dreamt', 'dreamed/dreamt', 'мечтать'],
+	['drink', 'drank', 'drunk', 'пить'],
+	['drive', 'drove', 'driven', 'ездить'],
+	['eat', 'ate', 'eaten', 'есть, поглощать'],
+	['fall', 'fell', 'fallen', 'падать'],
+	['feed', 'fed', 'fed', 'кормить'],
+	['feel', 'felt', 'felt', 'чувствовать'],
+	['fight', 'fought', 'fought', 'драться, сражаться'],
+	['find', 'found', 'found', 'находить'],
+	['fly', 'flew', 'flown', 'летать'],
+	['forget', 'forgot', 'forgotten', 'забывать'],
+	['forgive', 'forgave', 'forgiven', 'прощать'],
+	['freeze', 'froze', 'frozen', 'замерзать'],
+	['get', 'got', 'got', 'получать'],
+	['give', 'gave', 'given', 'давать'],
+	['go', 'went', 'gone/been', 'идти'],
+	['grow', 'grew', 'grown', 'расти'],
+	['hang', 'hung', 'hanged/hung', 'вешать'],
+	['have', 'had', 'had', 'иметь'],
+	['hear', 'heard', 'heard', 'слышать'],
+	['hide', 'hid', 'hidden', 'прятаться'],
+	['hit', 'hit', 'hit', 'ударять'],
+	['hold', 'held', 'held', 'держать'],
+	['hurt', 'hurt', 'hurt', 'причинять боль'],
+	['keep', 'kept', 'kept', 'держать, хранить'],
+	['kneel', 'knelt', 'knelt', 'становиться на колени'],
+	['know', 'knew', 'known', 'знать'],
+	['lay', 'laid', 'laid', 'лежать'],
+	['lead', 'led', 'led', 'вести, влиять, руководить'],
+	['learn', 'learned/learnt', 'learned/learnt', 'учиться'],
+	['leave', 'left', 'left', 'покидать, оставлять'],
+	['lend', 'lent', 'lent', 'одалживать, давать взаймы'],
+	['let', 'let', 'let', 'позволять'],
+	['lie', 'lay', 'lain', 'обманывать, лгать'],
+	['light', 'lit', 'lit', 'освещать'],
+	['lose', 'lost', 'lost', 'терять'],
+	['make', 'made', 'made', 'делать, создавать'],
+	['mean', 'meant', 'meant', 'означать'],
+	['meet', 'met', 'met', 'встречать'],
+	['pay', 'paid', 'paid', 'платить'],
+	['put', 'put', 'put', 'класть'],
+	['read', 'read', 'read', 'читать'],
+	['ride', 'rode', 'ridden', 'ехать верхом'],
+	['ring', 'rang', 'rung', 'звонить'],
+	['rise', 'rose', 'risen', 'восходить, поднимать'],
+	['run', 'ran', 'run', 'бежать'],
+	['say', 'said', 'said', 'говорить, произносить'],
+	['see', 'saw', 'seen', 'видеть'],
+	['sell', 'sold', 'sold', 'продавать'],
+	['send', 'sent', 'sent', 'отправлять'],
+	['set', 'set', 'set', 'устанавливать'],
+	['shake', 'shook', 'shaken', 'трясти'],
+	['shine', 'shone', 'shone', 'светить, озарять'],
+	['shoot', 'shot', 'shot', 'стрелять'],
+	['show', 'showed', 'shown', 'показывать'],
+	['shut', 'shut', 'shut', 'закрывать'],
+	['sing', 'sang', 'sung', 'петь'],
+	['sink', 'sank', 'sunk', 'тонуть, погружаться'],
+	['sit', 'sat', 'sat', 'сидеть'],
+	['sleep', 'slept', 'slept', 'спать'],
+	['slide', 'slid', 'slid', 'скользить'],
+	['smell', 'smelled/smelt', 'smelled/smelt', 'нюхать, пахнуть'],
+	['speak', 'spoke', 'spoken', 'говорить'],
+	['spend', 'spent', 'spent', 'тратить'],
+	['spill', 'spilled/spilt', 'spilled/spilt', 'проливать'],
+	['spoil', 'spoiled/spoilt', 'spoiled/spoilt', 'портить, наносить ущерб'],
+	['stand', 'stood', 'stood', 'стоять'],
+	['steal', 'stole', 'stolen', 'воровать, красть'],
+	['stick', 'stuck', 'stuck', 'вонзать, приклеивать'],
+	['swim', 'swam', 'swum', 'плавать'],
+	['take', 'took', 'taken', 'брать'],
+	['teach', 'taught', 'taught', 'учить'],
+	['tear', 'tore', 'torn', 'рвать'],
+	['tell', 'told', 'told', 'говорить, сообщать'],
+	['think', 'thought', 'thought', 'думать'],
+	['throw', 'threw', 'thrown', 'бросать'],
+	['understand', 'understood', 'understood', 'понимать'],
+	['wake', 'woke', 'woken', 'просыпаться, бодрствовать'],
+	['wear', 'wore', 'worn', 'носить'],
+	['win', 'won', 'won', 'выиграть'],
+	['write', 'wrote', 'written', 'писать'],
+	['can', 'could', 'been able', 'мочь, уметь, быть способным'],
+	['must', 'had to', 'had to', 'должен, обязан'],
+	['bet', 'bet', 'bet', 'делать ставку'],
+	['misunderstand', 'misunderstood', 'misunderstood', 'неправильно понимать'],
+	['broadcast', 'broadcast', 'broadcast', 'вещать (по радио, ТВ)'],
+];
 
 function generateVerb() {
-	$("#correctInput").html("");
-	
-	var vNumber = Math.round(Math.random() * (dictionarySize - 1)) + 1;
-	// Making sure the newly generated word is different from the previous one
-	while (vNumber == $("#vNumber").val()) {
-		Math.round(Math.random() * (dictionarySize - 1)) + 1;
-	}
-	var vForm = Math.round(Math.random() * 2);
-	var vRussian = v[vNumber][3];
+  hideAnswer();
 
-	$("#vNumber").val(vNumber);
-	$("#vForm").val(vForm);
-	$("#vRussian").text(vRussian);
+	index = Math.floor(Math.random() * dict.length);
+	form = Math.floor(Math.random() * 3);
 
-	var inputTag = "<input type='text' id='myInput' autocomplete='off'>";
+  var entry = dict[index];
 
-	if (vForm == 0) {
-		$("#vQ0").html(inputTag);
-		$("#vQ1").text(v[vNumber][1]);
-		$("#vQ2").text(v[vNumber][2]);
-	}
-	if (vForm == 1) {
-		$("#vQ0").text(v[vNumber][0]);
-		$("#vQ1").html(inputTag);
-		$("#vQ2").text(v[vNumber][2]);
-	}
-	if (vForm == 2) {
-		$("#vQ0").text(v[vNumber][0]);
-		$("#vQ1").text(v[vNumber][1]);
-		$("#vQ2").html(inputTag);
-	}
+  $('#first').text(entry[0]);
+  $('#second').text(entry[1]);
+  $('#third').text(entry[2]);
+  $('#russian').text(entry[3]);
 
-	$("#myInput").focus();
+  switch (form) {
+    case 0:
+      $('#first').html(inputTag);
+      break;
+    case 1:
+      $('#second').html(inputTag);
+      break;
+    default:
+    $('#third').html(inputTag);
+  }
+
+	$('#myInput').focus();
 }
 
 
 function checkVerb() {
-	var myInput = $("#myInput").val();
-	
-	// Correct input according to the dictionary
-	var correctInput0 = v[parseInt($("#vNumber").val())][parseInt($("#vForm").val())];
+	var myInput = $("#myInput").val().trim().toLowerCase();
+  myInput = '$' + myInput + '$';
 
-	myInput = $.trim(myInput);
-	myInput = myInput.toLowerCase();
-	myInput = myInput.replace(",","/").replace("-","/").replace(" /","/").replace("/ ","/");
+  var correctInput = '$' + dict[index][form].replace('/', '$') + '$';
+  var timer = 0;
 
-	// For some verbs, there are multiple correct versions ("learned/learnt"). If user wrote
-	// two words separated by "/", treat them as two different inputs.
-	// Using dollar signs to mark the beginning and the end of each verb
-	if (myInput.indexOf("/") === -1) {
-		myInput1 = "$" + myInput + "$";
-		myInput2 = "";
-	}
-	else {
-		var pos = myInput.indexOf("/");
-		myInput1 = "$" + myInput.slice(0, pos) + "$";
-		myInput2 = "$" + myInput.slice(pos + 1) + "$";
-	}
 
-	var correctInput = "$" + correctInput0 + "$";
-	var correctInput = correctInput.replace("/", "$");
+  if (correctInput.indexOf(myInput) > -1) {
+    correct++;
+    $("#myInput").css({backgroundColor: 'YellowGreen'});
+    timer = 1500;
+  } else {
+    $('#myInput').css({backgroundColor: 'DarkSalmon'});
+    showAnswer();
+    timer = 3000;
+  }
 
-	if ((correctInput.indexOf(myInput1, 0) !== -1) &&
-		(correctInput.indexOf(myInput2, 0) !== -1) &&
-		(myInput1 !== myInput2)) {	// correct! :D
-			$("#counterRight").text(parseInt( $("#counterRight").text() ) + 1);
-			$("#counterAll").text(parseInt( $("#counterAll").text() ) + 1);
-			$("#myInput").css({backgroundColor: "YellowGreen"});
-			t = setTimeout("generateVerb()", 1500); // generate new word in 1.5 sec
-	}
-	else {	// incorrect :(
-			$("#counterAll").text(parseInt( $("#counterAll").text() ) + 1);
-			$("#myInput").css({backgroundColor: "DarkSalmon"});
-			$("#correctInput").html("&nbsp;" + correctInput0 + "&nbsp;");
-			$("#verbAnswer").css({visibility: "visible"});
-			var t = setTimeout("generateVerb()", 3000);	// displaying correct verb for 3 seconds, then generating new verb
-	}	
+  setTimeout(generateVerb, timer);
+
+  updateCounters();
+
 }
 
+function updateCounters() {
+  $('#counterCorrect').text(correct);
+  $('#counterAltogether').text(altogether);
+}
+
+function showAnswer() {
+  $('#answer').text(dict[index][form]).show();
+}
+
+function hideAnswer() {
+  $('#answer').hide();
+}
 
 $(document).ready(function() {
 	generateVerb();
 
-	$("#questionForm").submit(function () {
+	$("form").submit(function () {
+    altogether++;
 		if ($("#myInput").attr("value") == "") {return false;}
-		$("#myInput").attr({disabled: "disabled"});
+		$("#myInput").attr({disabled: 'disabled'});
 		checkVerb();
 		return false;
 	});
